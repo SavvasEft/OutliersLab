b8dee940f06065d0103572885e230da2e15f84de
SavvasEft <savvas.eftychis@gmail.com>
2024-04-09 20:17:21 +0200

    added dictionary with methods in config, to be used instead of current decorator

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,8 +1,18 @@
 import os
 import sys
+
 current_directory =  os.path.abspath(os.path.dirname(__file__))
 grand_parent_directory = os.path.dirname(os.path.dirname(current_directory))
 sys.path.insert(0, grand_parent_directory) # defined root folder 1 step up
+
+from StatsMethods import get_z_score_iter_outliers_1d_mask, \
+                         get_iqr_outliers_1d_mask, \
+                         get_eucl_z_score_iterative_method_mas, \
+                         get_lof_outlier_mask
+
+from MLMethods import get_isolation_forest_outliers_mask, \
+                      get_lof_outlier_mask
+                         
 
 OUTPUT_FOLDER_PATH = os.path.join(grand_parent_directory, 'output')
 
@@ -18,8 +28,19 @@ N_NEIGHBORS = 24
 
 # Used for reminder, for raising exception and reminding to modify
 # method description in all places that it is used
+METHODS_DICT = {
+                'z-score' : get_z_score_iter_outliers_1d_mask,\
+                'IQR' : get_iqr_outliers_1d_mask,\
+                'eucl. z-score': get_eucl_z_score_iterative_method_mask,\
+                'Isol. Forest': get_isolation_forest_outliers_mask,\
+                'Local Outlier Factor': get_lof_outlier_mask
+}
+                         
+
+
 METHODS_DESCRIPTIONS = ['z-score', \
                         'IQR', \
+                        'eucl. z-score', \
                         'Isol. Forest', \
                         'Local Outlier Factor']
 
