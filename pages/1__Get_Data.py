cdde622128c874bc6313292a79a24495ba073a8b
SavvasEft <savvas.eftychis@gmail.com>
2024-04-30 22:07:10 +0200

    testing streamlit deployment, page 1, removing spaces

--- pages/1__Get_Data.py
+++ pages/1__Get_Data.py
@@ -23,7 +23,7 @@ from src.data_processing.DataGenerator import get_1d_distr_with_outliers, \
                                               
 from src.utils.toolbox import get_initial_1d_graphs, \
                               get_joint_plot_for_2d_data
-                              
+
 from src.utils.ReduceDimensions import reduce_to_2d_PCA
 
 
